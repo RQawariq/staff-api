df00be32e98c345b7cb62e58517c6eb5d646d769
RQawariq <rabeeqawariq@gmail.com>
2023-02-14 16:30:39 +0200

    edited

--- src/route/user/index.js
+++ src/route/user/index.js
@@ -2,8 +2,16 @@ import express from "express";
 
 const userRouter = express.Router();
 
-userRouter.get("/user", (req, res) => {
-  res.send({ success: true });
+userRouter.get("/user/login", async (req, res) => {
+  const { user, password } = req.query;
+  if (!user || !password) {
+    return res.send(`please enter both username and password`);
+  }
+  if (user === "test") {
+    return res.send(`hello ${user} & ${password}`);
+  } else {
+    return res.send("bad username");
+  }
 });
 
 export { userRouter };
