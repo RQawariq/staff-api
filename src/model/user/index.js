b0ed95d271e20dcdf5dc50ec17bb5e3885b76e2d
RQawariq <rabeeqawariq@gmail.com>
2023-02-16 19:15:46 +0200

    added user schema

---
+++ src/model/user/index.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+const usersSchema = new Schema({
+  name: String,
+  email: { type: String, unique: true, index: true },
+  gender: String,
+  password: String,
+  birthDay: Date,
+  country: String,
+  forgetPasswordToken: String,
+  coverPhoto: String,
+  profilePic: String,
+});
+usersSchema.index({ email: 1 });
+export const User = mongoose.model("User", usersSchema);
